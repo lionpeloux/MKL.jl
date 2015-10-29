931a27bbc0a878ac2f14e597de6df8462d0bc010
lionpeloux <lionel.dupeloux@gmail.com>
2015-10-30 00:01:03 +0100

    update mkllib ref

--- src/vml_service.jl
+++ src/vml_service.jl
@@ -1,7 +1,6 @@
 # ------------------------------------------------------------------------------
 #                              VML SERVICE FUNCTION
 # ------------------------------------------------------------------------------
-
 immutable VMLAccuracy
     mode::UInt
 end
@@ -13,8 +12,8 @@ const VML_EP = VMLAccuracy(0x00000003)
 Base.show(io::IO, m::VMLAccuracy) = print(io, m == VML_LA ? "VML_LA" :
                                               m == VML_HA ? "VML_HA" : "VML_EP")
 
-vml_get_mode() = ccall((:_vmlGetMode, lib), Cuint, ())
-vml_set_mode(mode::Integer) = (ccall((:_vmlSetMode, lib), Cuint, (UInt,), mode); nothing)
+vml_get_mode() = ccall((:_vmlGetMode, libmkl), Cuint, ())
+vml_set_mode(mode::Integer) = (ccall((:_vmlSetMode, libmkl), Cuint, (UInt,), mode); nothing)
 
 vml_set_accuracy(m::VMLAccuracy) = vml_set_mode((vml_get_mode() & ~0x03) | m.mode)
 vml_get_accuracy() = VMLAccuracy(vml_get_mode() & 0x3)
@@ -22,7 +21,7 @@ vml_get_accuracy() = VMLAccuracy(vml_get_mode() & 0x3)
 vml_set_mode((vml_get_mode() & ~0x0000FF00))
 
 function vml_check_error()
-    vml_error = ccall((:_vmlClearErrStatus, lib), Cint, ())
+    vml_error = ccall((:_vmlClearErrStatus, libmkl), Cint, ())
     if vml_error != 0
         if vml_error == 1
             error(DomainError())
